aa8f46e0a4dbf2a688a771c29dfe8ac890d8a79d
imcoolthanks <wongq9999@outlook.com>
2022-02-05 22:46:23 +0000

    Fixed Article, Card, Footer, Nav, Solutions

--- src/Componenets/Nav/Nav.js
+++ src/Componenets/Nav/Nav.js
@@ -4,17 +4,7 @@ import "./Nav.css";
 function Nav() {
   return (
     <nav className="nav">
-      <img
-        src="https://raw.githubusercontent.com/thinkingjet/PTC/dev/public/images/logo.png"
-        alt="img"
-        className="nav-logo"
-      />
       <ul className="nav-links">
-        <li>
-          <a href="https://raw.githubusercontent.com/thinkingjet/PTC/dev/public/images/logo.png">
-            Home
-          </a>
-        </li>
         <li>
           <a href="https://raw.githubusercontent.com/thinkingjet/PTC/dev/public/images/logo.png">
             About
@@ -22,7 +12,16 @@ function Nav() {
         </li>
         <li>
           <a href="https://raw.githubusercontent.com/thinkingjet/PTC/dev/public/images/logo.png">
-            Contribute
+          <img
+            src="https://raw.githubusercontent.com/thinkingjet/PTC/dev/public/images/logo.png"
+            alt="img"
+            className="nav-logo"
+          />
+          </a>
+        </li>
+        <li>
+          <a href="https://raw.githubusercontent.com/thinkingjet/PTC/dev/public/images/logo.png">
+            Contact
           </a>
         </li>
       </ul>
