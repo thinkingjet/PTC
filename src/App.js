32f570aa36f6f4ca6af617ff90d35f211530b584
imcoolthanks <wongq9999@outlook.com>
2022-02-05 13:22:44 +0000

    Improved cards

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import "./App.css";
 import Nav from "./Componenets/Nav/Nav";
 import Home from "./Componenets/Home/Home";
 import Info from "./Componenets/Info/Info";
+import Card from "./Componenets/Card/Card";
+import Footer from "./Componenets/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Home />
-      <Info />
+      <Card />
+      <Footer />
     </div>
   );
 }
