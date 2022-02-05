47ad2fe94624864658c922f4bec11295de4aa2cc
ssuryavanshi <ssuryavanshi@pop-os.localdomain>
2022-02-05 17:15:01 +0530

    pages updated

--- src/Componenets/Nav/Nav.js
+++ src/Componenets/Nav/Nav.js
@@ -5,23 +5,23 @@ function Nav() {
   return (
     <nav className="nav">
       <img
-        src="https://raw.githubusercontent.com/swwayam/donation/dev/public/images/logo.png"
+        src="https://raw.githubusercontent.com/thinkingjet/PTC/dev/public/images/logo.png"
         alt="img"
         className="nav-logo"
       />
       <ul className="nav-links">
         <li>
-          <a href="https://raw.githubusercontent.com/swwayam/donation/dev/public/images/logo.png">
+          <a href="https://raw.githubusercontent.com/thinkingjet/PTC/dev/public/images/logo.png">
             Home
           </a>
         </li>
         <li>
-          <a href="https://raw.githubusercontent.com/swwayam/donation/dev/public/images/logo.png">
+          <a href="https://raw.githubusercontent.com/thinkingjet/PTC/dev/public/images/logo.png">
             About
           </a>
         </li>
         <li>
-          <a href="https://raw.githubusercontent.com/swwayam/donation/dev/public/images/logo.png">
+          <a href="https://raw.githubusercontent.com/thinkingjet/PTC/dev/public/images/logo.png">
             Contribute
           </a>
         </li>
