1763c6b67ffcaa65f74aa0040800392232907e58
ssuryavanshi <ssuryavanshi@pop-os.localdomain>
2022-02-05 14:00:13 +0530

    Done with nav and home image

---
+++ src/Componenets/Home/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home-img">
+        <h1 className="home-img_heading">
+          Nature Provides A Free Lunch But Only If We <br /> Control Our
+          Appetites
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
