ed6b1a131622e33419d650f6f9a72731fe226d97
ssuryavanshi <ssuryavanshi@pop-os.localdomain>
2022-02-05 15:48:48 +0530

    Done with home

--- src/Componenets/Home/Home.js
+++ src/Componenets/Home/Home.js
@@ -5,10 +5,19 @@ function Home() {
   return (
     <div className="home">
       <div className="home-img">
-        <h1 className="home-img_heading">
+        <h2 className="home-img_heading">CHANGE LIVES. SAVE LIVES.</h2>
+        <h3 className="home-img_subtitle_line">
+          Help us make a difference And reduce, resue and recycle stuff.
+        </h3>
+        <h4 className="home-img_quote">
           Nature Provides A Free Lunch But Only If We <br /> Control Our
           Appetites
-        </h1>
+        </h4>
+        <h4 className="home-img_author">-William Ruckelshaus</h4>
+      </div>
+      <div className="home-btns">
+        <button className="home-btn">How can you help</button>
+        <button className="home-btn home-btn-1">Ways to contribute</button>
       </div>
     </div>
   );
