bcfa28b40da48dea865194ac04bd173807a597e5
imcoolthanks <wongq9999@outlook.com>
2022-02-05 12:37:43 +0000

    First commite

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import Nav from "./Componenets/Nav/Nav";
+import Home from "./Componenets/Home/Home";
+import Info from "./Componenets/Info/Info";
+import Footer from "./Componenets/Footer/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Home />
+      <Info />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
