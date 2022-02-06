920c4e6bc9944e857b27bbae07d9986a096fa9e9
ssuryavanshi <ssuryavanshi@pop-os.localdomain>
2022-02-06 15:57:41 +0530

    hi

--- src/Componenets/Nav/Nav.js
+++ src/Componenets/Nav/Nav.js
@@ -4,19 +4,18 @@ import "./Nav.css";
 function Nav() {
   return (
     <nav className="nav">
+      <a href="#">
+        <img
+          src="https://raw.githubusercontent.com/thinkingjet/PTC/dev/public/images/logo.png"
+          alt="img"
+          className="nav-logo"
+        />
+      </a>
       <ul className="nav-links">
         <li>
           <a href="#">About</a>
         </li>
-        <li>
-          <a href="#">
-            <img
-              src="https://raw.githubusercontent.com/thinkingjet/PTC/dev/public/images/logo.png"
-              alt="img"
-              className="nav-logo"
-            />
-          </a>
-        </li>
+        <li></li>
         <li>
           <a href="#">Donate</a>
         </li>
