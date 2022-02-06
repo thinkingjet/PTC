d9ff3b04e0a8015538776e6a2781c56082987f31
ssuryavanshi <ssuryavanshi@pop-os.localdomain>
2022-02-06 19:54:31 +0530

    Home conflit resolved

--- src/Componenets/Nav/Nav.js
+++ src/Componenets/Nav/Nav.js
@@ -4,7 +4,6 @@ import "./Nav.css";
 function Nav() {
   return (
     <nav className="nav">
-<<<<<<< HEAD
       <div>
         <a href="#" className="nav-logo">
           cL
@@ -24,31 +23,13 @@ function Nav() {
         </ul>
       </div>
       <div className="auth">
-=======
-      <a href="#">
-        <img
-          src="https://raw.githubusercontent.com/thinkingjet/PTC/dev/public/images/logo.png"
-          alt="img"
-          className="nav-logo"
-        />
-      </a>
-      <ul className="nav-links">
->>>>>>> home
         <li>
           <a href="#">Login</a>
         </li>
-<<<<<<< HEAD
         <li>
           <a href="#">Signup</a>
         </li>
       </div>
-=======
-        <li></li>
-        <li>
-          <a href="#">Donate</a>
-        </li>
-      </ul>
->>>>>>> home
     </nav>
   );
 }
